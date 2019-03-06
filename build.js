4cdeaddd10e78af8e062dd265fc41d7fef3c1aa7
keleko34 <Gregory.Guidero@gmail.com>
2019-03-06 01:08:47 +0100

    update init with IE fix

--- build.js
+++ build.js
@@ -10,4 +10,11 @@ flags.compilationLevel = 'SIMPLE';
 
 fs.writeFileSync(base+'/pikantny.min.js',closureCompiler(flags).compiledCode);
 
+/* Temporary backwards compatibility */
+flags = {};
+flags.jsCode = [{src: fs.readFileSync(base+'/init.js','utf8')}];
+flags.compilationLevel = 'SIMPLE';
+
+fs.writeFileSync(base+'/init.min.js',closureCompiler(flags).compiledCode);
+
 console.log("Finished Building Minified Library..");
